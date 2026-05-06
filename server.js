3c3ce2e3ef3ae5a002bba8d629cad25ad8c4aded
trivedinj-beep <trivedinj@vcu.edu>
2026-05-05 22:41:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(__dirname));
+
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
